367d0197ee42334ed7dcedc84790d36836a9b594
Anaise <a.umugwanez@alustend.com>
2026-06-16 13:05:07 +0200

    add user input prompt with empty, space, and duplicate directory validation

--- setup_project.sh
+++ setup_project.sh
@@ -1,3 +1,27 @@
 #!/bin/bash
 
 #This is a script that creates directory structure, writes source files, configures thresholds, validates the environment, and handles SIGINT.
+
+USER_INPUT=""
+PROJECT_DIR=""
+
+#Get project name from the user
+echo "--------DIRECTORY ARCHITECTURE--------"
+while true; do
+	read -p "Enter a project tag (e.g. v1): " USER_INPUT
+	if [-z "$USER_INPUT"]; then
+		echo "Tag cannot be empty. Please try again."
+		continue
+	fi
+	if [[ "$USER_INPUT" =~ [[:space:]] ]]; then
+        	echo "Tag cannot contain spaces. Use underscores or dashes instead."
+		continue
+	fi
+	break
+done
+PROJECT_DIR="attendance_tracker_${USER_INPUT}"
+if [ -d "$PROJECT_DIR" ]; then
+    echo "Sorry, directory '$PROJECT_DIR' already exists."
+    echo "Choose a different tag or remove the existing directory first."
+    exit 1
+fi
